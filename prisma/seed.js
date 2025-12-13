91be50d5414bbaeebe8cede62d2e0ad6a4416768
yeeuneey <yeeuneey@naver.com>
2025-12-14 01:50:39 +0900

    ㅈㅂㅈㅂ

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,10 +2,16 @@
 require("dotenv").config();
 const bcrypt = require("bcrypt");
 const { PrismaClient } = require("@prisma/client");
+const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
+const mariadb = require("mariadb");
 
 const SALT = 10;
 
-const prisma = new PrismaClient();
+const poolUrl =
+  (process.env.DATABASE_URL || "").replace(/^mysql:\/\//i, "mariadb://");
+const pool = mariadb.createPool(poolUrl);
+const adapter = new PrismaMariaDb(pool);
+const prisma = new PrismaClient({ adapter });
 
 /* -----------------------------------------
    Helper Functions
--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,5 +1,13 @@
+require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
+const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
+const mariadb = require("mariadb");
 
-const prisma = new PrismaClient();
+// Shared MariaDB/MySQL pool via Prisma adapter
+const poolUrl =
+  (process.env.DATABASE_URL || "").replace(/^mysql:\/\//i, "mariadb://");
+const pool = mariadb.createPool(poolUrl);
+const adapter = new PrismaMariaDb(pool);
+const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
