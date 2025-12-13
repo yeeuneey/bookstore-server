cdfc735b41a8f3960c70234c786eb5c7315584a1
yeeuneey <yeeuneey@naver.com>
2025-12-14 00:59:20 +0900

    db

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,16 +1,5 @@
-require('dotenv').config();
-const { PrismaClient } = require('@prisma/client');
-const { PrismaMariaDb } = require('@prisma/adapter-mariadb');
+const { PrismaClient } = require("@prisma/client");
 
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-  throw new Error('DATABASE_URL is not set in the environment.');
-}
-
-// MariaDB adapter works with MySQL protocol servers (e.g., MySQL 8)
-const adapter = new PrismaMariaDb(databaseUrl);
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 module.exports = prisma;
