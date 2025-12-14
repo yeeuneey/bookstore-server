e69a23fea0eeefc7a0e972f16ffbad43cc5b0105
yeeuneey <yeeuneey@naver.com>
2025-12-14 21:14:30 +0900

    44..

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,10 +1,7 @@
 require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
-const { MariaDbAdapter } = require("@prisma/adapter-mariadb");
-const mariadb = require("mariadb");
+const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
 
-// MariaDB driver expects the scheme to be "mariadb://".
-// Allow users to provide either mysql:// or mariadb://.
 const rawUrl = process.env.DATABASE_URL;
 const connectionUrl = rawUrl?.replace(/^mysql:\/\//i, "mariadb://");
 
@@ -12,12 +9,8 @@ if (!connectionUrl) {
   throw new Error("DATABASE_URL is not set");
 }
 
-// Create a MariaDB/MySQL pool using the normalized connection string.
-const pool = mariadb.createPool(connectionUrl);
-
-// PrismaClient with the MariaDB adapter (required for Prisma 7+ when URL is in prisma.config.ts).
-const prisma = new PrismaClient({
-  adapter: new MariaDbAdapter(pool),
-});
+// Prisma 7+ with datasource URL in prisma.config.ts requires an adapter.
+const adapter = new PrismaMariaDb(connectionUrl);
+const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
